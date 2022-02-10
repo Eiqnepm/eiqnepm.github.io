fc7e374ae367a650b25a8b41ea5e194393232acb
Eiqnepm <86803173+Eiqnepm@users.noreply.github.com>
2022-02-10 21:01:10 +0000

    Update username.js

--- js/username.js
+++ js/username.js
@@ -1,7 +1,13 @@
-(async function () {
+async function generate() {
   const response = await fetch(
     "https://www.reddit.com/api/v1/generate_username.json"
   );
+  
+  if (response.status != 200) {
+    setTimeout(generate, 10 * 1000);
+    return;
+  }
+  
   const data = await response.json();
   const table = document.getElementById("table");
 
@@ -10,4 +16,6 @@
     const cell = row.insertCell(-1);
     cell.innerHTML = name;
   }
-})();
+}
+
+generate();
